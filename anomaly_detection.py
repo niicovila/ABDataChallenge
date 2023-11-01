e44a273c825b406a0799e90bfa4f1185080e7be8
Nicolas Vila <nicolasvila@Nicolass-iMac.technicolor.net>
2023-11-01 23:26:15 +0100

    RCF anomalies

--- anomaly_detection.py
+++ anomaly_detection.py
@@ -7,18 +7,11 @@ import matplotlib.pyplot as plt
 import os
 
 
-def read_data(file_path):
-    df = pd.read_csv(file_path)
-    return df
-
-def prepare_data(df):
-    return 0
-
 def main():
     # Create a directory to store results
     os.makedirs('anomaly_results', exist_ok=True)
     file_path = '/Users/nicolasvila/workplace/uni/ABData/abdataset1_barcelona_temp_seq.csv'
-    original_data = read_data(file_path)
+    original_data = pd.read_csv(file_path)
     # Assuming 'original_data' is your DataFrame
     original_data['Type of economic activity'] = original_data['Type of economic activity'].str.replace('*', '')
 
