1ec5b777fdfa840dd488c0707b10130b5c168a98
Nicolas Vila <nicolasvila@Nicolass-iMac.technicolor.net>
2023-11-01 23:24:58 +0100

    RCF anomalies

--- anomaly_detection.py
+++ anomaly_detection.py
@@ -1,22 +1,34 @@
-#%%
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import StandardScaler
+import rrcf
+import heapq
+import matplotlib.pyplot as plt
+import os
+
 
 def read_data(file_path):
     df = pd.read_csv(file_path)
     return df
 
 def prepare_data(df):
-
     return 0
-#%%
+
 def main():
-    #%%
+    # Create a directory to store results
+    os.makedirs('anomaly_results', exist_ok=True)
     file_path = '/Users/nicolasvila/workplace/uni/ABData/abdataset1_barcelona_temp_seq.csv'
     original_data = read_data(file_path)
-    #%%
+    # Assuming 'original_data' is your DataFrame
+    original_data['Type of economic activity'] = original_data['Type of economic activity'].str.replace('*', '')
+
+    # Now all '*' characters are removed from the 'Type of economic activity' column
+
     # Group the DataFrame by 'District', 'Use', and 'Type of economic activity'
     grouped_data = original_data.groupby(['District', 'Use', 'Type of economic activity'])
+    
+    unique_combinations = grouped_data[['Use', 'Type of economic activity']].nunique().shape[0]
+    print("Number of Unique Combinations of Use and Type of Economic Activity:", unique_combinations)
 
     # Create a dictionary to store dataframes for each district and unique combination of 'Use' and 'Type of economic activity'
     district_activity_dataframes = {}
@@ -24,120 +36,122 @@ def main():
     # Iterate through grouped data and create separate dataframes for each district and activity combination
     for (district, use, activity), data in grouped_data:
         key = f'{district}_{use}_{activity}'  # Unique key for each combination
-        print(key)
+        district, use, activity = key.split('_')
+        
+        district_dir = os.path.join('anomaly_results', district)
+        os.makedirs(district_dir, exist_ok=True)
+
+        subdir = os.path.join(district_dir, f'{use}_{activity.replace('*', '')}')
+        os.makedirs(subdir, exist_ok=True)
+        
         district_activity_dataframes[key] = data.copy()
         district_activity_dataframes[key]['Date'] = pd.to_datetime(district_activity_dataframes[key][['Year', 'Month', 'Day']])
         district_activity_dataframes[key] = district_activity_dataframes[key].sort_values(by='Date', ascending=True)
-    #%%
-    district_activity_dataframes['1_Commercial_ * DIY items'].head()
-    district_activity_dataframes['1_Commercial_ * DIY items']['Date'] = pd.to_datetime(district_activity_dataframes['1_Commercial_ * DIY items'][['Year', 'Month', 'Day']])
-    district_activity_dataframes['1_Commercial_ * DIY items'] = district_activity_dataframes['1_Commercial_ * DIY items'].sort_values(by='Date', ascending=True)
-    #%%
-    district_activity_dataframes['1_Commercial_ * DIY items'].head()
-    #%%
-    df_sorted =  district_activity_dataframes['1_Commercial_ * DIY items']
-    #%%
-    df_encoded = pd.get_dummies(df_sorted, columns=['District', 'Postcode', 'Use', 'Type of economic activity'])
-    #%%
-    df_encoded.head()
-    #%%
-    data = df_encoded.values
-    #%%
-    type(data)
-    data[0]
-    #%%
-    features = ['Year', 'Month', 'Day' ,'Number of meters', 'Consumption (L/Day)', 'Precipitaciones (mm)', 'T_max (C)', 'T_min (C)', 'spi_12', 'spi_9', 'scpdsi', 'District_1',	'Postcode_8001',	'Postcode_8002',	'Postcode_8003',	'Postcode_8039','Use_Commercial',	'Type of economic activity_ * DIY items']
-    from sklearn.preprocessing import StandardScaler
-    import rrcf
+        df_encoded = pd.get_dummies(district_activity_dataframes[key], columns=['District', 'Postcode', 'Use', 'Type of economic activity'])
+        df_encoded = df_encoded.drop(columns=['Date'])
 
-    # Set tree parameters
-    num_trees = 40
-    shingle_size = 4
-    tree_size = 256
+        data = df_encoded.values
+        scaler = StandardScaler()
+        data = scaler.fit_transform(data)
+        # Set tree parameters
+        num_trees = 10
+        shingle_size = 5
+        tree_size = 256
 
-    # Create a forest of empty trees
-    forest = []
-    for _ in range(num_trees):
-        tree = rrcf.RCTree()
-        forest.append(tree)
+        # Create a forest of empty trees
+        forest = []
+        for _ in range(num_trees):
+            tree = rrcf.RCTree()
+            forest.append(tree)
+            
+        # Use the "shingle" generator to create rolling window
+        points = rrcf.shingle(data, size=shingle_size)
+        # Create a dict to store anomaly score of each point
+        avg_codisp = {}
+
+        # For each shingle...
+        for index, point in enumerate(points):
+            # For each tree in the forest...
+            for tree in forest:
+                # If tree is above permitted size...
+                if len(tree.leaves) > tree_size:
+                    # Drop the oldest point (FIFO)
+                    tree.forget_point(index - tree_size)
+                # Insert the new point into the tree
+                tree.insert_point(point, index=index)
+                # Compute codisp on the new point...
+                new_codisp = tree.codisp(index)
+                # And take the average over all trees
+                if not index in avg_codisp:
+                    avg_codisp[index] = 0
+                avg_codisp[index] += new_codisp / num_trees
+                
+        consumption = []
+        anomaly_score = []
+        for row in data:
+            consumption.append(row[4])
+        for i in avg_codisp.keys():
+            anomaly_score.append(avg_codisp[i])
+
+        # Why there is always a 9 row difference --> it is because of 'shingle size' in the model. 
+        print(len(anomaly_score))
+        print(len(district_activity_dataframes[key]))
+
+        top_anomaly_positions = heapq.nlargest(5, range(len(anomaly_score)), key=lambda i: anomaly_score[i])
+        df_encoded['Date'] = pd.to_datetime(district_activity_dataframes[key][['Year', 'Month', 'Day']])
+    
+        selected_rows = df_encoded.iloc[top_anomaly_positions]
+        selected_rows.to_csv(os.path.join(subdir, 'top_anomalies_info.csv'), index=False)
+    
+        df_encoded.set_index('Date', inplace=True)
+        # Save the distribution plot as an image
+        plt.figure(figsize=(8, 6))
+        plt.hist(anomaly_score, bins=30, edgecolor='black')
+        plt.xlabel('Anomaly Score')
+        plt.ylabel('Frequency')
+        plt.title('Distribution of Anomaly Scores')
+        plt.savefig(os.path.join(subdir, 'anomaly_score_distribution.png'))
+        plt.close()
         
-    # Use the "shingle" generator to create rolling window
-    points = rrcf.shingle(data, size=shingle_size)
-    #%%
-    # Create a dict to store anomaly score of each point
-    avg_codisp = {}
+        # Save the time series plot as an image
+        plt.figure(figsize=(12, 6))
+        plt.semilogy(df_encoded.index, df_encoded['Consumption (L/Day)'], label='Log Consumption (L/Day)')
+        plt.plot(df_encoded.iloc[:-shingle_size+1].index, anomaly_score, label='Anomaly Score (x1000)')
+        plt.xlabel('Time Step')
+        plt.ylabel('Consumption (L/Day)')
+        plt.title('Time-Dependent Data with Anomaly Scores')
+        plt.legend()
+        plt.savefig(os.path.join(subdir, 'time_series_plot.png'))
+        plt.close()
+        break
 
-    # For each shingle...
-    for index, point in enumerate(points):
-        # For each tree in the forest...
-        for tree in forest:
-            # If tree is above permitted size...
-            if len(tree.leaves) > tree_size:
-                # Drop the oldest point (FIFO)
-                tree.forget_point(index - tree_size)
-            # Insert the new point into the tree
-            tree.insert_point(point, index=index)
-            # Compute codisp on the new point...
-            new_codisp = tree.codisp(index)
-            # And take the average over all trees
-            if not index in avg_codisp:
-                avg_codisp[index] = 0
-            avg_codisp[index] += new_codisp / num_trees
-    #%%
-    consumption = []
-    anomaly_score = []
-    for row in data:
-        consumption.append(row[4])
-    for key in avg_codisp.keys():
-        anomaly_score.append(avg_codisp[key])
+   
+        # Print the positions of the top 5 highest anomalies
+        # print("Positions of Top 5 Highest Anomalies:")
+        # print(top_anomaly_positions)
+        # for index in top_anomaly_positions:
+        #     print(anomaly_score[index])
 
-    import heapq
+        # selected_rows = df_encoded.iloc[top_anomaly_positions]
+        # selected_rows.head()
 
-    # Your existing code here...
+        # plt.figure(figsize=(12, 6))
+        # plt.plot(consumption, label='Consumption (L/Day)')
+        # plt.plot(anomaly_score, label='Anomaly Score')
+        # plt.xlabel('Time Step')
+        # plt.ylabel('Consumption (L/Day)')
+        # plt.title('Time-Dependent Data with Anomaly Scores')
+        # plt.legend()
+        # plt.show()
 
-    # Find the indices of the top 5 anomalies with highest scores
-    top_anomaly_positions = heapq.nlargest(5, range(len(anomaly_score)), key=lambda i: anomaly_score[i])
+        # plt.figure(figsize=(8, 6))
+        # plt.hist(anomaly_score, bins=30, edgecolor='black')
+        # plt.xlabel('Anomaly Score')
+        # plt.ylabel('Frequency')
+        # plt.title('Distribution of Anomaly Scores')
+        # plt.show()
 
-    # Print the positions of the top 5 highest anomalies
-    print("Positions of Top 5 Highest Anomalies:")
-    print(top_anomaly_positions)
-    for index in top_anomaly_positions:
-        print(anomaly_score[index])
 
-    #%%
-    selected_rows = df_encoded.iloc[top_anomaly_positions]
-    selected_rows.head()
-    #%%
-    import matplotlib.pyplot as plt
-    # Plot the time-dependent data along with the corresponding anomaly scores
-    plt.figure(figsize=(12, 6))
-    plt.plot(consumption, label='Consumption (L/Day)')
-    #plt.plot(anomaly_score, label='Anomaly Score')
-    plt.xlabel('Time Step')
-    plt.ylabel('Consumption (L/Day)')
-    plt.title('Time-Dependent Data with Anomaly Scores')
-    plt.legend()
-    plt.show()
-    #%%
-    plt.figure(figsize=(8, 6))
-    plt.hist(anomaly_score, bins=30, edgecolor='black')
-    plt.xlabel('Anomaly Score')
-    plt.ylabel('Frequency')
-    plt.title('Distribution of Anomaly Scores')
-    plt.show()
-
-    #%%
-    anomaly_scores = []
-    indices = []
-    for index, point in enumerate(points):
-        # Your existing processing code...
-
-        # Store the average anomaly score and its index
-        anomaly_scores.append(avg_codisp[index])
-        indices.append(index)
-    print(anomaly_scores)
-    #%%
-    #%%
     # # Standardize features
     # scaler = StandardScaler()
     # scaled_features = scaler.fit_transform(df_encoded[features])
@@ -170,29 +184,34 @@ def main():
 
     # print(len(anomaly_dates))
 
+################################################################################################
+     ################ CODE TO REORDER TIME DATA ####################
+################################################################################################
+    # import pandas as pd
 
+    # # Assuming df_sorted is your DataFrame sorted by 'Date' in ascending order
+    # # Assuming 'Date' is a datetime column
+    # # Reset the index to use row indices for accessing the data
+    # df_sorted.reset_index(drop=True, inplace=True)
 
-    #%%
-    import pandas as pd
+    # # Initialize an empty list to store rows of the new DataFrame
+    # new_rows = []
 
-    # Assuming df_sorted is your DataFrame sorted by 'Date' in ascending order
-    # Assuming 'Date' is a datetime column
-    # Reset the index to use row indices for accessing the data
-    df_sorted.reset_index(drop=True, inplace=True)
-
-    # Initialize an empty list to store rows of the new DataFrame
-    new_rows = []
-
-    # Iterate through the DataFrame to create rows for the new DataFrame
-    for i in range(10, len(df_sorted)):
-        current_row = df_sorted.iloc[i]
-        past_rows = df_sorted.iloc[i-10:i]
+    # # Iterate through the DataFrame to create rows for the new DataFrame
+    # for i in range(10, len(df_sorted)):
+    #     current_row = df_sorted.iloc[i]
+    #     past_rows = df_sorted.iloc[i-10:i]
         
-        # Extract data and dates for the last 10 days for each variable
-        consumption_data = [(past_rows.at[i - j, 'Consumption (L/Day)'], past_rows.at[i - j, 'Date']) for j in range(10, 0, -1)]
+    #     # Extract data and dates for the last 10 days for each variable
+    #     consumption_data = [(past_rows.at[i - j, 'Consumption (L/Day)'], past_rows.at[i - j, 'Date']) for j in range(10, 0, -1)]
         
-        print(consumption_data)
-        break
+    #     print(consumption_data)
+    #     break
+    ################################################################################################################################################
+
+################################################################################################################################################
+
+
     #     precipitation_data = [(past_rows.at[i - j, 'Precipitaciones (mm)'], past_rows.at[i - j, 'Date']) for j in range(1, 11)]
     #     tmax_data = [(past_rows.at[i - j, 'T_max (C)'], past_rows.at[i - j, 'Date']) for j in range(1, 11)]
     #     tmin_data = [(past_rows.at[i - j, 'T_min (C)'], past_rows.at[i - j, 'Date']) for j in range(1, 11)]
@@ -282,11 +301,6 @@ def main():
     #     # Add the new row to the list
     #     new_rows.append(new_row)
 
-    # # Create a new DataFrame from the list of rows
-    # new_df = pd.DataFrame(new_rows)
 
-    # # Now, new_df contains the pivoted data with columns representing the past 10 days' data for each variable
-    # print(new_df)
-    #%%
 if __name__ == '__main__':
     main()
