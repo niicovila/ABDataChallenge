4c3611bca3f4e9293fef1fb0bac0092fb5560e34
Nicolas Vila <nicolas.vila01@estudiant.upf.edu>
2023-11-04 18:51:45 +0100

    rrcf model

--- anomaly_detection_rcf.py
+++ anomaly_detection_rcf.py
@@ -2,129 +2,152 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 import rrcf
-import heapq
+import argparse
 import matplotlib.pyplot as plt
 import os
+from prepare_data import prepare_data_ae
 
-#TODO: Change code so that we ask the user for an input DISTRICT, USE and TYPE OF ECONOMIC ACTIVITY
 
-def main():
-    # Create a directory to store results
-    os.makedirs('anomaly_results', exist_ok=True)
-    file_path = '/Users/nicolasvila/workplace/uni/ABData/abdataset1_barcelona_temp_seq.csv'
-    original_data = pd.read_csv(file_path)
-    # Assuming 'original_data' is your DataFrame
-    original_data['Type of economic activity'] = original_data['Type of economic activity'].str.replace('*', '')
-
-    # Now all '*' characters are removed from the 'Type of economic activity' column
-    distinct_activities = original_data['Type of economic activity'].unique()
-
-    # Write distinct values to a text file
-    with open(os.path.join('', 'distinct_activities.txt'), 'w') as file:
-        for i, activity in enumerate(distinct_activities):
-            file.write(f'{i}. {activity}' + '\n')
-
-    # Group the DataFrame by 'District', 'Use', and 'Type of economic activity'
-    grouped_data = original_data.groupby(['District', 'Use', 'Type of economic activity'])
+def rrcf_anomalies(path):
+    try:
     
-    unique_combinations = grouped_data[['Use', 'Type of economic activity']].nunique().shape[0]
-    print("Number of Unique Combinations of Use and Type of Economic Activity:", unique_combinations)
-
-    # Create a dictionary to store dataframes for each district and unique combination of 'Use' and 'Type of economic activity'
-    district_activity_dataframes = {}
-
-    # Iterate through grouped data and create separate dataframes for each district and activity combination
-    for (district, use, activity), data in grouped_data:
-        key = f'{district}_{use}_{activity}'  # Unique key for each combination
-        district, use, activity = key.split('_')
+        # Create a directory to store results
+        os.makedirs('anomaly_results', exist_ok=True)
+        original_data = pd.read_csv(path)
+        original_data['Type of economic activity'] = original_data['Type of economic activity'].str.replace('*', '')
         
-        district_dir = os.path.join('anomaly_results', district)
-        os.makedirs(district_dir, exist_ok=True)
+        tensor_data, dataframes = prepare_data_ae(original_data)
+        # Iterate through grouped data and create separate dataframes for each district and activity combination
+        uses = {'0': 'Commercial', '1': 'Industrial'}
+        # Get unique values from 'Type of economic activity' column
 
-        subdir = os.path.join(district_dir, f'{use}_{activity.replace("*", "")}')
-        os.makedirs(subdir, exist_ok=True)
+
+        unique_activities = original_data['Type of economic activity'].unique()
+        # Create a dictionary with indices as keys and unique activities as values
+        activities = {str(i): activity for i, activity in enumerate(unique_activities)}
+
+        districts = list(range(1, 11))
+
+        use = input("Enter use (0 for Commercial, 1 for Industrial): ")
+        activity = input("Enter activity index: ")
+        district = input("Enter district: ")
+        percentile = 0.99
+        percentile = (input("Choose a percentile to include anomalies based on anomaly score distribution"))
+        if percentile.isnumeric():
+            # Convert percentile to float
+            percentile = float(percentile)
+        else:
+            # Handle the case when percentile is not numeric (optional)
+            print("Percentile wasn't numeric.")
+
+        if use not in uses.keys() or activity not in activities.keys() or int(district) not in districts:
+            print("Invalid input. Please provide valid use, activity, and district.")
+        else:
+            key = f'{district}_{uses[use]}_{activities[activity]}'
+            print("Key:", key)
         
-        district_activity_dataframes[key] = data.copy()
-        district_activity_dataframes[key]['Date'] = pd.to_datetime(district_activity_dataframes[key][['Year', 'Month', 'Day']])
-        district_activity_dataframes[key] = district_activity_dataframes[key].sort_values(by='Date', ascending=True)
-        df_encoded = pd.get_dummies(district_activity_dataframes[key], columns=['District', 'Postcode', 'Use', 'Type of economic activity'])
-        df_encoded = df_encoded.drop(columns=['Date'])
-
-        data = df_encoded.values
-        scaler = StandardScaler()
-        data = scaler.fit_transform(data)
-        # Set tree parameters
-        num_trees = 10
-        shingle_size = 5
-        tree_size = 256
-
-        # Create a forest of empty trees
-        forest = []
-        for _ in range(num_trees):
-            tree = rrcf.RCTree()
-            forest.append(tree)
+        if key in dataframes.keys():
+            scaler = StandardScaler()
+            dataframe = dataframes[key]
+            dataframe['Consumption per meter (L/Day)'] = scaler.fit_transform(dataframe[['Consumption per meter (L/Day)']])
+            dataframe.reset_index(drop=True, inplace=True)
             
-        # Use the "shingle" generator to create rolling window
-        points = rrcf.shingle(data, size=shingle_size)
-        # Create a dict to store anomaly score of each point
-        avg_codisp = {}
-
-        # For each shingle...
-        for index, point in enumerate(points):
-            # For each tree in the forest...
-            for tree in forest:
-                # If tree is above permitted size...
-                if len(tree.leaves) > tree_size:
-                    # Drop the oldest point (FIFO)
-                    tree.forget_point(index - tree_size)
-                # Insert the new point into the tree
-                tree.insert_point(point, index=index)
-                # Compute codisp on the new point...
-                new_codisp = tree.codisp(index)
-                # And take the average over all trees
-                if not index in avg_codisp:
-                    avg_codisp[index] = 0
-                avg_codisp[index] += new_codisp / num_trees
-                
-        consumption = []
-        anomaly_score = []
-        for row in data:
-            consumption.append(row[4])
-        for i in avg_codisp.keys():
-            anomaly_score.append(avg_codisp[i])
-
-        # Why there is always a 9 row difference --> it is because of 'shingle size' in the model. 
-        print(len(anomaly_score))
-        print(len(district_activity_dataframes[key]))
-
-        top_anomaly_positions = heapq.nlargest(5, range(len(anomaly_score)), key=lambda i: anomaly_score[i])
-        df_encoded['Date'] = pd.to_datetime(district_activity_dataframes[key][['Year', 'Month', 'Day']])
-    
-        selected_rows = df_encoded.iloc[top_anomaly_positions]
-        selected_rows.to_csv(os.path.join(subdir, 'top_anomalies_info.csv'), index=False)
-    
-        df_encoded.set_index('Date', inplace=True)
-        # Save the distribution plot as an image
-        plt.figure(figsize=(8, 6))
-        plt.hist(anomaly_score, bins=30, edgecolor='black')
-        plt.xlabel('Anomaly Score')
-        plt.ylabel('Frequency')
-        plt.title('Distribution of Anomaly Scores')
-        plt.savefig(os.path.join(subdir, 'anomaly_score_distribution.png'))
-        plt.close()
+            district, use, activity = key.split('_')
+            district_dir = os.path.join('anomaly_results', district)
+            os.makedirs(district_dir, exist_ok=True)
+            subdir = os.path.join(district_dir, f'{use}_{activity.replace("*", "")}')
+            os.makedirs(subdir, exist_ok=True)
+            dataframe.drop(columns=['Date'], inplace=True)
+            
         
-        # Save the time series plot as an image
-        plt.figure(figsize=(12, 6))
-        plt.semilogy(df_encoded.index, df_encoded['Consumption (L/Day)'], label='Log Consumption (L/Day)')
-        plt.plot(df_encoded.iloc[:-shingle_size+1].index, anomaly_score, label='Anomaly Score')
-        plt.xlabel('Time Step')
-        plt.ylabel('Consumption (L/Day)')
-        plt.title('Time-Dependent Data with Anomaly Scores')
-        plt.legend()
-        plt.savefig(os.path.join(subdir, 'time_series_plot.png'))
-        plt.close()
-        break
+            # Set tree parameters
+            data = dataframe.values
+            num_trees = 100
+            shingle_size = 10
+            tree_size = 512
+            time_series_length = len(dataframe)
+            # Create a forest of empty trees
+            forest = []
+            for _ in range(num_trees):
+                tree = rrcf.RCTree()
+                forest.append(tree)
+                
+            # Use the "shingle" generator to create rolling window
+            points = rrcf.shingle(data, size=shingle_size)
+            # Create a dict to store anomaly score of each point
+            avg_codisp = {}
+
+            # For each shingle...
+            for index, point in enumerate(points):
+                # For each tree in the forest...
+                for tree in forest:
+                    # If tree is above permitted size...
+                    if len(tree.leaves) > tree_size:
+                        # Drop the oldest point (FIFO)
+                        tree.forget_point(index - tree_size)
+                    # Insert the new point into the tree
+                    tree.insert_point(point, index=index)
+                    # Compute codisp on the new point...
+                    new_codisp = tree.codisp(index)
+                    # And take the average over all trees
+                    if not index in avg_codisp:
+                        avg_codisp[index] = 0
+                    avg_codisp[index] += new_codisp / num_trees
+                    
+            consumption = []
+            anomaly_score = []
+            for row in data:
+                consumption.append(row[4])
+            for i in avg_codisp.keys():
+                anomaly_score.append(avg_codisp[i])
+
+        
+            percentile_threshold = np.percentile(anomaly_score, 95)
+            selected_anomaly_positions = [position for position, score in enumerate(anomaly_score) if score > percentile_threshold]
+        
+            anomaly_windows = []
+            for position in selected_anomaly_positions:
+                start_position = max(0, position - shingle_size + 1)  # Ensure start position is within the bounds
+                end_position = min(position + 1, time_series_length)  # Ensure end position is within the bounds
+                anomaly_windows.append((start_position, end_position))
+
+            dataframe['Date'] = pd.to_datetime(dataframe[['Year', 'Month', 'Day']])
+        
+            selected_rows = dataframe.iloc[selected_anomaly_positions]
+            selected_rows.to_csv(os.path.join(subdir, 'top_anomalies_info.csv'), index=False)
+            
+            # Save the distribution plot as an image
+            plt.figure(figsize=(8, 6))
+            plt.hist(anomaly_score, bins=30, edgecolor='black')
+            plt.axvline(x=percentile_threshold, color='red', linestyle='dashed', linewidth=2, label='95th Percentile')
+            plt.xlabel('Anomaly Score')
+            plt.ylabel('Frequency')
+            plt.title('Distribution of Anomaly Scores')
+            plt.savefig(os.path.join(subdir, 'anomaly_score_distribution.png'))
+            plt.close()
+            
+            # Save the time series plot as an image
+            plt.figure(figsize=(12, 6))
+            plt.plot(dataframe['Date'], dataframe['Consumption per meter (L/Day)'], label='Consumption per meter (L/Day)')
+        
+            for window in anomaly_windows:
+                plt.axvspan(dataframe['Date'].iloc[window[0]], dataframe['Date'].iloc[window[1] - 1], facecolor='red', alpha=0.3)
+
+            plt.xlabel('Time Step')
+            plt.ylabel('Consumption (L/Day)')
+            plt.title('Time-Dependent Data with Anomaly Scores and Detected Anomalies')
+            plt.legend()
+            plt.show()
+            plt.savefig(os.path.join(subdir, 'time_series_plot_with_anomalies.png'))
+            plt.close()
+
+    except Exception as e:
+        print(f"Error: {e}")
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process data anomalies based on percentile.')
+    parser.add_argument('path', type=str, help='Path to the CSV file')
+
+    args = parser.parse_args()
+    rrcf_anomalies(args.path)
--- prepare_data.py
+++ prepare_data.py
@@ -1,17 +1,11 @@
-#%%
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 #TODO: Change code so that we ask the user for an input DISTRICT, USE and TYPE OF ECONOMIC ACTIVITY
-#%%
-def prepare_data_ae(path):
-    #%%
-    path = '/Users/nicolasvila/workplace/uni/ABData/abdataset1_barcelona_temp_seq.csv'
-    original_data = pd.read_csv(path)
-    #%%
-    original_data['Type of economic activity'] = original_data['Type of economic activity'].str.replace('*', '')
-  
+
+def prepare_data_ae(original_data):
     distinct_activities = original_data['Type of economic activity'].unique()
     original_data['Consumption per meter (L/Day)'] = original_data['Consumption (L/Day)'] /original_data['Number of meters']
     columns_to_drop = ['Postcode', 'Consumption (L/Day)', 'Number of meters']
@@ -19,15 +13,14 @@ def prepare_data_ae(path):
 
     # Group the DataFrame by 'District', 'Use', and 'Type of economic activity'
     grouped_data = original_data.groupby(['District', 'Use', 'Type of economic activity'])
-    
+
     district_activity_dataframes = {}
     reshaped_data_list = []
-    
+    dataframes = []
     for (district, use, activity), data in grouped_data:
         key = f'{district}_{use}_{activity}'  
         district, use, activity = key.split('_')
     
-        
         district_activity_dataframes[key] = data.copy()
         district_activity_dataframes[key]['Date'] = pd.to_datetime(district_activity_dataframes[key][['Year', 'Month', 'Day']])
         district_activity_dataframes[key] = district_activity_dataframes[key].sort_values(by='Date', ascending=True)
@@ -39,9 +32,9 @@ def prepare_data_ae(path):
         df_encoded['District'] = label_encoder.fit_transform(df_encoded['District'])
         df_encoded['Use'] = label_encoder.fit_transform(df_encoded['Use'])
         df_encoded['Type of economic activity'] = label_encoder.fit_transform(df_encoded['Type of economic activity'])
-
-        df_encoded.head()
-        grouped_data_2 = df_encoded.groupby('Date').agg({
+        
+        df_encoded.reset_index(inplace=True)
+        df_encoded = df_encoded.groupby('Date').agg({
             'District': 'first',
             'Year': 'first',
             'Month': 'first',
@@ -56,15 +49,16 @@ def prepare_data_ae(path):
             'spi_12': 'mean',
             'spi_9': 'mean',
             'scpdsi': 'mean'
-        })
-        print(df_encoded.head())
-        data = grouped_data_2.values
-        print(data.shape[0])
+        }).reset_index(drop=True)
+   
+        data = df_encoded.values
+        district_activity_dataframes[key] = df_encoded
         if (data.shape[0]) == 1460 :
-    
             reshaped_data_list.append(data)
+
     reshaped_data = np.stack(reshaped_data_list, axis=0)
-    print(reshaped_data.shape)
+
+    return reshaped_data, district_activity_dataframes
 
 if __name__ == '__main__':
     path = '/Users/nicolasvila/workplace/uni/ABData/abdataset1_barcelona_temp_seq.csv'
