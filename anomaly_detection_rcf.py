0fefae4d31745983321426b76d15cf6b61a4b662
niicovila <nicolas.vila01@estudiant.upf.edu>
2023-11-05 13:55:40 +0100

    .

--- anomaly_detection_rcf.py
+++ anomaly_detection_rcf.py
@@ -14,21 +14,24 @@ def rrcf_anomalies(path):
         # Create a directory to store results
         os.makedirs('anomaly_results', exist_ok=True)
         original_data = pd.read_csv(path)
-        original_data['Type of economic activity'] = original_data['Type of economic activity'].str.replace('*', '')
+        original_data['Type of economic activity'] = original_data['Type of economic activity']
         
         tensor_data, dataframes = prepare_data_ae(original_data)
         
         # Iterate through grouped data and create separate dataframes for each district and activity combination
         uses = {'0': 'Commercial', '1': 'Industrial'}
         unique_activities = original_data['Type of economic activity'].unique()
+        unique_postcodes = original_data['Postcode'].unique()
+        postcodes = {str(i): postcode for i, postcode in enumerate(unique_postcodes)}
+        
         activities = {str(i): activity for i, activity in enumerate(unique_activities)}
-        districts = list(range(1, 11))
+        # districts = list(range(1, 11)) --> CHANGE TO POSTCODE
 
-        district = input("Enter district: ")
+        # district = input("Enter POSTCODE: ")
         use = input("Enter use (0 for Commercial, 1 for Industrial): ")
         activity = input("Enter activity index: ")
-        
-        percentile = 99
+        postcode = input("Enter POSTCODE index:  ")
+        percentile = 95
         
         # percentile = (input("Choose a percentile to include anomalies based on anomaly score distribution"))
         # if percentile.isnumeric():
@@ -38,12 +41,13 @@ def rrcf_anomalies(path):
         #     # Handle the case when percentile is not numeric (optional)
         #     print("Percentile wasn't numeric.")
 
-        if use not in uses.keys() or activity not in activities.keys() or int(district) not in districts:
+        if use not in uses.keys() or activity not in activities.keys() or postcode not in postcodes.keys():
             print("Invalid input. Please provide valid use, activity, and district.")
+            return 0
         else:
-            key = f'{district}_{uses[use]}_{activities[activity]}'
+            key = f'{postcodes[postcode]}_{uses[use]}_{activities[activity]}'
             print("Key:", key)
-        print(len(dataframes.keys()))
+
         if key in dataframes.keys():
             print('Computing anomalies ...')
             scaler = StandardScaler()
@@ -51,16 +55,17 @@ def rrcf_anomalies(path):
             dataframe['Consumption per meter (L/Day)'] = scaler.fit_transform(dataframe[['Consumption per meter (L/Day)']])
             dataframe.reset_index(drop=True, inplace=True)
             
-            district, use, activity = key.split('_')
-            district_dir = os.path.join('anomaly_results', district)
-            os.makedirs(district_dir, exist_ok=True)
-            subdir = os.path.join(district_dir, f'{use}_{activity.replace("*", "")}')
+            postcode, use, activity = key.split('_')
+            postcode_dir = os.path.join('anomaly_results', postcode)
+            os.makedirs(postcode_dir, exist_ok=True)
+            subdir = os.path.join(postcode_dir, f'{use}_{activity.replace("*", "")}')
             os.makedirs(subdir, exist_ok=True)
-            dataframe.drop(columns=['Date'], inplace=True)
+            df_nodate = dataframe.copy()
+            df_nodate.drop(columns=['Date'], inplace=True)
             
         
             # Set tree parameters
-            data = dataframe.values
+            data = df_nodate.values
             num_trees = 100
             shingle_size = 10
             tree_size = 512
@@ -92,28 +97,31 @@ def rrcf_anomalies(path):
                     if not index in avg_codisp:
                         avg_codisp[index] = 0
                     avg_codisp[index] += new_codisp / num_trees
-                    
+            print("Finished computing anomalies\n")  
             consumption = []
             anomaly_score = []
             for row in data:
-                consumption.append(row[4])
+                consumption.append(row[3])
             for i in avg_codisp.keys():
                 anomaly_score.append(avg_codisp[i])
-
-        
+            
             percentile_threshold = np.percentile(anomaly_score, percentile)
+            print("Selecting anomalies")
+            
+
             selected_anomaly_positions = [position for position, score in enumerate(anomaly_score) if score > percentile_threshold]
         
+            print("Finding anomaly windows")
             anomaly_windows = []
             for position in selected_anomaly_positions:
-                start_position = max(0, position - shingle_size + 1)  # Ensure start position is within the bounds
-                end_position = min(position + 1, time_series_length)  # Ensure end position is within the bounds
+                start_position = max(0, position)  # Ensure start position is within the bounds
+                end_position = min(position + shingle_size-1, time_series_length)  # Ensure end position is within the bounds
                 anomaly_windows.append((start_position, end_position))
 
-            dataframe['Date'] = pd.to_datetime(dataframe[['Year', 'Month', 'Day']])
+            #dataframe['Date'] = pd.to_datetime(dataframe[['Year', 'Month', 'Day']])
         
             formatted_dates = '\n'.join([f"{dataframe['Date'].iloc[anomaly_windows[i][0]]} <--> {dataframe['Date'].iloc[anomaly_windows[i][1] - 1]}" for i in range(len(anomaly_windows))])
-            result = f'''With the selected percentile, we found a total of {len(anomaly_windows)} for the selected district, use, and economic activity.\nThe anomalies were found between the following dates:\n{formatted_dates}'''
+            result = f'''With the selected percentile, we found a total of {len(anomaly_windows)} for the selected postcode, use, and economic activity.\nThe anomalies were found between the following dates:\n\n{formatted_dates}'''
             with open(os.path.join(subdir, 'result.txt'), 'w') as file:
                 file.write(result)
             
@@ -136,8 +144,8 @@ def rrcf_anomalies(path):
                 plt.axvspan(dataframe['Date'].iloc[window[0]], dataframe['Date'].iloc[window[1] - 1], facecolor='red', alpha=0.3)
 
             plt.xlabel('Time Step')
-            plt.ylabel('Consumption (L/Day)')
-            plt.title('Time-Dependent Data with Anomaly Scores and Detected Anomalies')
+            plt.ylabel('Consumption per meter (L/Day) - normalised')
+            plt.title('Consumption with Detected Anomalies')
             plt.legend()
             plt.savefig(os.path.join(subdir, 'time_series_plot_with_anomalies.png'))
             plt.show()
--- prepare_data.py
+++ prepare_data.py
@@ -6,57 +6,54 @@ from sklearn.preprocessing import LabelEncoder
 def prepare_data_ae(original_data):
     distinct_activities = original_data['Type of economic activity'].unique()
     original_data['Consumption per meter (L/Day)'] = original_data['Consumption (L/Day)'] /original_data['Number of meters']
-    columns_to_drop = ['Postcode', 'Consumption (L/Day)', 'Number of meters']
+    columns_to_drop = ['Consumption (L/Day)', 'Number of meters']
     original_data = original_data.drop(columns=columns_to_drop)
 
     # Group the DataFrame by 'District', 'Use', and 'Type of economic activity'
-    grouped_data = original_data.groupby(['District', 'Use', 'Type of economic activity'])
+    grouped_data = original_data.groupby(['Postcode', 'Use', 'Type of economic activity'])
 
-    district_activity_dataframes = {}
+    postcode_activity_dataframes = {}
     reshaped_data_list = []
-    dataframes = []
-    for (district, use, activity), data in grouped_data:
-        key = f'{district}_{use}_{activity}'  
-        district, use, activity = key.split('_')
+    for (postcode, use, activity), data in grouped_data:
+        key = f'{postcode}_{use}_{activity}'  
+        postcode, use, activity = key.split('_')
     
-        district_activity_dataframes[key] = data.copy()
-        district_activity_dataframes[key]['Date'] = pd.to_datetime(district_activity_dataframes[key][['Year', 'Month', 'Day']])
-        district_activity_dataframes[key] = district_activity_dataframes[key].sort_values(by='Date', ascending=True)
+        postcode_activity_dataframes[key] = data.copy()
+        #postcode_activity_dataframes[key]['Date'] = pd.to_datetime(postcode_activity_dataframes[key][['Year', 'Month', 'Day']])
+        postcode_activity_dataframes[key] = postcode_activity_dataframes[key].sort_values(by='Date', ascending=True)
        
         label_encoder = LabelEncoder()
 
-        df_encoded = district_activity_dataframes[key]
+        df_encoded = postcode_activity_dataframes[key]
        
-        df_encoded['District'] = label_encoder.fit_transform(df_encoded['District'])
+        df_encoded['Postcode'] = label_encoder.fit_transform(df_encoded['Postcode'])
         df_encoded['Use'] = label_encoder.fit_transform(df_encoded['Use'])
         df_encoded['Type of economic activity'] = label_encoder.fit_transform(df_encoded['Type of economic activity'])
         
         df_encoded.reset_index(inplace=True)
         df_encoded = df_encoded.groupby('Date').agg({
-            'District': 'first',
-            'Year': 'first',
-            'Month': 'first',
-            'Day':'first',
+            'Postcode': 'first',
             'Use': 'first',
             'Type of economic activity':'first',
             'Consumption per meter (L/Day)': 'max',
-            'Precipitaciones (mm)': 'mean',  
-            'Date' : 'first', 
-            'T_max (C)': 'mean',
-            'T_min (C)': 'mean',  
+            # 'Precipitaciones (mm)': 'mean',  
+            # 
+            'Date' : 'first',
+            # 'T_max (C)': 'mean',
+            # 'T_min (C)': 'mean',  
             'spi_12': 'mean',
             'spi_9': 'mean',
             'scpdsi': 'mean'
         }).reset_index(drop=True)
    
         data = df_encoded.values
-        district_activity_dataframes[key] = df_encoded
+        postcode_activity_dataframes[key] = df_encoded
         if (data.shape[0]) == 1460 :
             reshaped_data_list.append(data)
 
     reshaped_data = np.stack(reshaped_data_list, axis=0)
 
-    return reshaped_data, district_activity_dataframes
+    return reshaped_data, postcode_activity_dataframes
 
 if __name__ == '__main__':
     path = '/Users/nicolasvila/workplace/uni/ABData/abdataset1_barcelona_temp_seq.csv'
